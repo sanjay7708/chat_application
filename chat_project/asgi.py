60d82d2adb755e27a32343095e7c02385ec028e6
sanjay7708 <sanjaysri1310@gmail.com>
2026-02-10 16:56:25 +0530

    chagne asgi.py file

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -7,17 +7,30 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
+
 import os
-from channels.auth import AuthMiddlewareStack
+import django
+
+# ✅ tell Django which settings to use
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "chat_project.settings"
+)
+
+# ✅ initialize Django BEFORE anything else
+django.setup()
+
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.middleware import JWTAuthMiddleware
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
+import chat.routing
 
 django_application = get_asgi_application()
 
-import chat.routing
-application=ProtocolTypeRouter({
-    'http':django_application,
-    'websocket':JWTAuthMiddleware(URLRouter(chat.routing.websocket_urlpatterns))
+application = ProtocolTypeRouter({
+    "http": django_application,
+    "websocket": JWTAuthMiddleware(
+        URLRouter(chat.routing.websocket_urlpatterns)
+    ),
 })
+
