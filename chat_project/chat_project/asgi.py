836503110536e73e4f28710cc5c3dc72edfa272f
Sanjay <82385800+sanjay7708@users.noreply.github.com>
2026-02-12 16:03:25 +0530

    Refactor ASGI application setup in asgi.py

--- chat_project/chat_project/asgi.py
+++ chat_project/chat_project/asgi.py
@@ -1,13 +1,13 @@
 import os
-
+from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_project.settings")
 
-from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.middleware import JWTAuthMiddleware
 import chat.routing
 
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
